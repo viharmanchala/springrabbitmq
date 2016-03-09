05e9de43421e692431b9c15154ae4a67b635ddbd
Vihar <vihar.manchala@bnl-consulting.com>
2016-03-09 10:26:22 -0500

    project

---
+++ spring rabbitmq/src/main/java/com/memorynotfound/rabbitmq/Consumer.java
@@ -0,0 +1,11 @@
+package com.memorynotfound.rabbitmq;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+public class Consumer implements MessageListener {
+
+    public void onMessage(Message message) {
+        System.out.println(new String(message.getBody()));
+    }
+}
